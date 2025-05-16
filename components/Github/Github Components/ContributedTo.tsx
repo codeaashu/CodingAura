5afe5854292f284863862c67785a65978a646487
Madhu Kalita 🫐 <157983984+madhukalita@users.noreply.github.com>
2025-05-16 12:00:43 +0530

    <🗿>

--- components/Github/Github Components/ContributedTo.tsx
+++ components/Github/Github Components/ContributedTo.tsx
@@ -16,7 +16,7 @@ const ContributedTo = ({
     >
       {/* <HalfCircleGradient  /> */}
       <Image
-        src={`/assets/grad11.svg`}
+        src={`/gcontri.jpeg`}
         alt=""
         width={500}
         height={500}
