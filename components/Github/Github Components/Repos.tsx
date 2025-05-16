aa3fe3b16241e2c85a3a494782911cb772369157
ashutoshpythoncs@gmail.com <ashutoshpythoncs@gmail.com>
2025-05-16 11:05:17 +0530

    <🗿>

--- components/Github/Github Components/Repos.tsx
+++ components/Github/Github Components/Repos.tsx
@@ -10,7 +10,7 @@ const Repos = ({ repos, classname }: { repos: number; classname: string }) => {
     >
       {/* <HalfCircleGradient  /> */}
       <Image
-        src={`/assets/pr.jpeg`}
+        src={`/assets/repo.jpeg`}
         alt=""
         width={500}
         height={500}
